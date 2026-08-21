338b3fd8151ad98abbe688565fb0f861b276be7b
iBellaco <barbadiego695@gmail.com>
2026-08-20 22:39:22 -0400

    ci: optimizar pipeline de github actions con action-gh-release v2 y artefacto apk directo (v1.72)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.aistudio.wildriftdrafting.wrdftx"
     minSdk = 24
     targetSdk = 36
-    versionCode = 72
-    versionName = "1.71"
+    versionCode = 73
+    versionName = "1.72"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
