6bd825af30c93b440751143b226786aeece02db9
iBellaco <barbadiego695@gmail.com>
2026-08-26 12:11:31 -0400

    git commit -m "ci: corregir pipeline de GitHub Actions agregando setup-android, aceptacion de licencias para SDK 36 y disparadores globales"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
     applicationId = "com.aistudio.wildriftdrafting.wrdftx"
     minSdk = 24
     targetSdk = 36
-    versionCode = 57
-    versionName = "1.3.38"
+    versionCode = 58
+    versionName = "1.3.39"
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
 
