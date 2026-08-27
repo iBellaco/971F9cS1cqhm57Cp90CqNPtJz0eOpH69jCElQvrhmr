504f184aa5f07f75f456b4ae6e5f89ed26c5c9e6
iBellaco <barbadiego695@gmail.com>
2026-08-27 12:55:43 -0400

    git commit -m "fix(memory): fragmentar lista gigante de 141 campeones en chunks más pequeños para evitar fallos de memoria en el compilador de GitHub Actions y corregir tipado"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
     applicationId = "com.aistudio.wildriftdrafting.wrdftx"
     minSdk = 24
     targetSdk = 36
-    versionCode = 113
-    versionName = "1.3.94"
+    versionCode = 114
+    versionName = "1.3.95"
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
 
