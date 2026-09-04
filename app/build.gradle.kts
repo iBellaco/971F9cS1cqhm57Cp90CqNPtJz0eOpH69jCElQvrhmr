3a6c1b3af7dfe01bd3b7b2eab00d096fc082a071
iBellaco <barbadiego695@gmail.com>
2026-09-04 00:53:12 -0400

    test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
     applicationId = "com.aistudio.wildriftdrafting.wrdftx"
     minSdk = 24
     targetSdk = 36
-    versionCode = 1106
-    versionName = "1.1.06"
+    versionCode = 1107
+    versionName = "1.1.07"
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
 
