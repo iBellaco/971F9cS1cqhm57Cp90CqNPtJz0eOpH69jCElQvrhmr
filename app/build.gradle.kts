a235be8dd72ba2c7912e73e751824e94b942d95c
iBellaco <barbadiego695@gmail.com>
2026-09-06 19:09:14 -0400

    chore(release): bump version to 1.2.7
    Update versionCode to 100 and versionName to 1.2.7 for the new release.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
     applicationId = "com.aistudio.wildriftdrafting.wrdftx"
     minSdk = 24
     targetSdk = 36
-    versionCode = 99
-    versionName = "1.2.6"
+    versionCode = 100
+    versionName = "1.2.7"
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
 
