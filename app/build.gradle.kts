7e8f26f0e0466ca2d126b081304fee689ae5b8e0
iBellaco <barbadiego695@gmail.com>
2026-09-07 09:08:57 -0400

    chore: bump version to 1.3.0
    Update versionCode to 112 and versionName to 1.3.0.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
     applicationId = "com.aistudio.wildriftdrafting.wrdftx"
     minSdk = 24
     targetSdk = 36
-    versionCode = 111
-    versionName = "1.2.18"
+    versionCode = 112
+    versionName = "1.3.0"
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
 
