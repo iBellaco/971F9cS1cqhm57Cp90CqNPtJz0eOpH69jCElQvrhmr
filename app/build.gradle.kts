bea3c54cadb14cb6ccf489961efd07ee0dd4d135
iBellaco <barbadiego695@gmail.com>
2026-09-09 22:41:40 -0400

    feat: asegurar compatibilidad universal del escaner de draft en cualquier modelo de celular y actualizar a v1.3.95

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
     applicationId = "com.aistudio.wildriftdrafting.wrdftx"
     minSdk = 24
     targetSdk = 36
-    versionCode = 206
-    versionName = "1.3.94"
+    versionCode = 207
+    versionName = "1.3.95"
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
 
