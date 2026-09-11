50b3b47a89be9c6b9249c6b5c73d100e58a4c4a9
iBellaco <barbadiego695@gmail.com>
2026-09-11 17:45:43 -0400

    ci(github-actions): agregar flujo de trabajo para purgar releases antiguos y conservar únicamente el más reciente

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
     applicationId = "com.aistudio.wildriftdrafting.wrdftx"
     minSdk = 24
     targetSdk = 36
-    versionCode = 282
-    versionName = "1.9.20"
+    versionCode = 283
+    versionName = "1.9.21"
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
 
